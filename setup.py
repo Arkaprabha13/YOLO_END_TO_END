e52be4259c789be5d9993a80e6b249381f7a9f56
Arkaprabha Banerjee <arkaofficial13@gmail.com>
2024-04-05 22:16:57 +0530

    req.txt run done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="YOLO_END_TO_END",
+    version="0.0.0",
+    author="Arkaprabha Banerjee",
+    author_email="arkaofficial13@gmail.com",
+    description="A package for end to end YOLO implementation",
+        
+    packages=find_packages(),
+    install_requires=[]
+)
