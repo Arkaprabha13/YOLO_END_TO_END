1bc54bf42b4ddb3b9e2482a6a7a5afd1fee394b8
Arkaprabha Banerjee <arkaofficial13@gmail.com>
2024-04-06 09:52:24 +0530

    notebook,logger,exception and utils updated

---
+++ app.py
@@ -0,0 +1,10 @@
+from YOLO_ENT_TO_END.logger import logging
+from YOLO_ENT_TO_END.exception import AppException
+import sys
+
+# logging.info("Hello World")
+
+# try: 
+#     a=3/0
+# except Exception as e:
+#     raise AppException(e, sys) from e
